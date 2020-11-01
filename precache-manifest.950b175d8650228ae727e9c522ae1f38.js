80b41307b1abfdeee02f11703c9e0e01772e3211
SyahdiAkbar99 <58156854+SyahdiAkbar99@users.noreply.github.com>
2020-10-31 21:17:56 -0700

    commit salin
    React js

---
+++ precache-manifest.950b175d8650228ae727e9c522ae1f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42eb23fb214cebd9a8e4",
+    "url": "/static/css/main.9672c37d.chunk.css"
+  },
+  {
+    "revision": "42eb23fb214cebd9a8e4",
+    "url": "/static/js/main.42eb23fb.chunk.js"
+  },
+  {
+    "revision": "be2a2dea8672164fe856",
+    "url": "/static/js/1.be2a2dea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a68b10b70321606019b04c437edf5beb",
+    "url": "/static/media/sego.a68b10b7.jpg"
+  },
+  {
+    "revision": "fbe1033c7e62c91f18165ef6d2876d34",
+    "url": "/index.html"
+  }
+];
